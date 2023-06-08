513e03f0c8a338525fe19206a187558cfcffae84
aashish2057 <aashish2057@gmail.com>
2023-06-08 08:37:35 -0500

    setup ts server

--- lua/aashish/plugins/lsp/lspconfig.lua
+++ lua/aashish/plugins/lsp/lspconfig.lua
@@ -43,6 +43,11 @@ lspconfig["cssls"].setup({
 	on_attach = on_attach,
 })
 
+lspconfig["tsserver"].setup({
+	capabilities = capabilities,
+	on_attach = on_attach,
+})
+
 lspconfig["tailwindcss"].setup({
 	capabilities = capabilities,
 	on_attach = on_attach,
